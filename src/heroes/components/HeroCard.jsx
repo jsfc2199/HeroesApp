ec4ba6e936bb234c6a4c270d8440d7c0c39e9338
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-10 10:47:16 -0500

    reestructurado la hero card

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,5 +1,12 @@
 /* eslint-disable react/prop-types */
 
+import { Link } from "react-router-dom";
+
+const CharactersByHero = ({ alter_ego, characters }) => {
+  if (alter_ego === characters) return <></>;
+  return <p>{characters}</p>;
+};
+
 export const HeroCard = ({
   id,
   superhero,
@@ -8,27 +15,31 @@ export const HeroCard = ({
   first_appearance,
   characters,
 }) => {
-    const heroImgUrl = `/assets/heroes/${id}.jpg`
+  const heroImgUrl = `/assets/heroes/${id}.jpg`;
 
   return (
-  <div className="col">
-    <div className="card">
+    <div className="col">
+      <div className="card">
         <div className="row no-gutters">
-            <div className="col-4">
-                <img src={heroImgUrl} className="card-img" alt={superhero} />
+          <div className="col-4">
+            <img src={heroImgUrl} className="card-img" alt={superhero} />
+          </div>
+          <div className="col-8">
+            <div className="card-body">
+              <h5 className="card-title">{superhero}</h5>
+              <p className="card-text">{alter_ego}</p>
+
+              <CharactersByHero characters={characters} alter_ego={alter_ego} />
+
+              <p className="card-text">
+                <small className="text-muted">{first_appearance}</small>
+              </p>
+
+              <Link to={`/hero/${id}`}>Mas Informacion</Link>
             </div>
-            <div className="col-8">
-              <div className="card-body">
-                <h5 className="card-title">{superhero}</h5>
-                <p className="card-text">{alter_ego}</p>
-                <p>{characters}</p>
-              </div>
-            </div>
+          </div>
         </div>
+      </div>
     </div>
-
-  </div>)
-  
+  );
 };
-
-
