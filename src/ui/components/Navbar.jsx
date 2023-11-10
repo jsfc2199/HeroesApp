b2701bcd25cafd1b538d26f5edf62d3b8634444a
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-10 10:09:39 -0500

    Uso de navigate para viajar entre rutas

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,16 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const navigate = useNavigate(); //ayuda a la navegacion
+
+    const onLogout = () => {
+        navigate('/login',{
+            replace: true
+        });
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -36,7 +45,7 @@ export const Navbar = () => {
                     <span className='nav-item nav-link text-primary'>
                         Juan
                     </span>
-                    <button className='nav-item nav-link btn'>
+                    <button className='nav-item nav-link btn' onClick={onLogout}>
                         Logout
                     </button>
                 </ul>
