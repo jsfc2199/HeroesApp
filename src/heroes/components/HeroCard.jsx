234d8644f0d74312992b981f58bcc5fc8b54ede8
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-14 10:15:51 -0500

    añadidas animaciones

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -19,10 +19,10 @@ export const HeroCard = ({
   const heroImgUrl = `/assets/heroes/${id}.jpg`;
 
   return (
-    <div className="col">
+    <div className="col animate__animated animate__fadeIn">
       <div className="card">
         <div className="row no-gutters">
-          <div className="col-4">
+          <div className="col-4" >
             <img src={heroImgUrl} className="card-img" alt={superhero} />
           </div>
           <div className="col-8">
