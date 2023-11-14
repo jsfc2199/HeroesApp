5e001c30ab2c48eabccde8e763db314daacdc8f7
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-14 10:29:27 -0500

    diseño preliminar del componente de buscar un heroe

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,43 @@
+import { HeroCard } from './../components/HeroCard';
 export const SearchPage = () => {
-  return (
-    <div>SearchPage</div>
-  )
-}
 
+  return (
+    <>
+      <h1>Search</h1>
+      <hr />
+
+      <div className="row">
+      <div className="col-4">
+        <h4>Searching</h4>
+        <hr />
+        <form action="">
+          <input
+            type="text"
+            placeholder="search a hero"
+            name="searchText"
+            autoComplete="off"
+          />
+        </form>
+
+        <button className="btn btn-outline-primary mt-1">Search</button>
+      </div>
+
+      <div className="col-7">
+
+        <h4>Results</h4>
+        <hr />
+        <div className="alert alert-primary">
+          Search a Hero
+        </div>
+
+        <div className="alert alert-danger">
+          No hero with <b>ABC</b>
+        </div>
+
+        <HeroCard />
+
+      </div>
+      </div>
+    </>
+  );
+};
