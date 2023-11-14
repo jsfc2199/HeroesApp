0c4468c2c850df75d1f2395c405ca0034d6ccc89
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-14 10:43:47 -0500

    se implementa query-string para usar query parameters son use location y useNavigate

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,42 +1,59 @@
-import { HeroCard } from './../components/HeroCard';
-export const SearchPage = () => {
+import { useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "../../hook/useForm";
+import { HeroCard } from "./../components/HeroCard";
+import queryString from "query-string";
 
+export const SearchPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation(); //para leer los query parameters
+  const { q = "" } = queryString.parse(location.search);
+
+  const { searchText, onInputChange } = useForm({
+    searchText: "",
+  });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    if (searchText.trim().length <= 1) return;
+
+    //query parameters
+    navigate(`?q=${searchText}`);
+  };
+  
   return (
     <>
       <h1>Search</h1>
       <hr />
 
       <div className="row">
-      <div className="col-4">
-        <h4>Searching</h4>
-        <hr />
-        <form action="">
-          <input
-            type="text"
-            placeholder="search a hero"
-            name="searchText"
-            autoComplete="off"
-          />
-        </form>
+        <div className="col-4">
+          <h4>Searching</h4>
+          <hr />
+          <form onSubmit={onSearchSubmit}>
+            <input
+              type="text"
+              placeholder="search a hero"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
+            />
+          </form>
 
-        <button className="btn btn-outline-primary mt-1">Search</button>
-      </div>
-
-      <div className="col-7">
-
-        <h4>Results</h4>
-        <hr />
-        <div className="alert alert-primary">
-          Search a Hero
+          <button className="btn btn-outline-primary mt-1">Search</button>
         </div>
 
-        <div className="alert alert-danger">
-          No hero with <b>ABC</b>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+          <div className="alert alert-primary">Search a Hero</div>
+
+          <div className="alert alert-danger">
+            No hero with <b>{q}</b>
+          </div>
+
+          <HeroCard />
         </div>
-
-        <HeroCard />
-
-      </div>
       </div>
     </>
   );
