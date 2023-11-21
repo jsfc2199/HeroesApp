509610d5ee30929c3a9596dbee3b9e4108303739
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-21 18:43:31 -0500

    Protección de rutas
     Se almacena usuario en el local storage para persistir la información del usuario

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,25 +3,37 @@ import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authReducer";
 import { types } from './../types/types';
 
-const initialState = {
-    logged: false
+// const initialState = {
+//     logged: false
+// }
+
+//inicializador del estado del reducer
+const init = () => {
+   const user = JSON.parse( localStorage.getItem('user'))
+   return {
+    logged: !!user,
+    user: user
+   }
 }
 
 
 export const AuthProvider = ({ children }) => {
 
-  const [authState, dispatch] =  useReducer(authReducer, initialState)
+  const [authState, dispatch] =  useReducer(authReducer, {}, init) //podemos obviar el initialState ya que se lee desde el localStorage
 
   //funcion que al llamarla ejecuta el dispatch
   const login = (name = '') => {
 
+    const user = {
+        id: 'ABC',
+        name: name
+    }
     const action = {
         type: types.login,
-        payload: {
-            id: 'ABC',
-            name: name
-        }
+        payload: user
     }
+
+    localStorage.setItem('user', JSON.stringify(user)) //guardamos el usuario en el local storage
 
     dispatch(action)
   }
