f02a0d2cabb20913fa2dc404c449b0c56b1fa097
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-30 18:34:24 -0500

    Pruebas unitarias
    Se hacen pruebas en el auth reducer

---
+++ tests(auth/context/authReducer.test.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+import { types } from "../../src/auth/types/types";
+import { authReducer } from "./../../src/auth/context/authReducer";
+describe("Pruebas en authReducer", () => {
+  test("debe de retornar el estado por defecto ", () => {
+    const state = authReducer({ logged: false }, {});
+    expect(state).toEqual({ logged: false });
+  });
+
+  test("debe de llamar el login auth y establecer el usuario ", () => {
+    const action = {
+      type: types.login,
+      payload: {
+        name: "Juan",
+        id: "123",
+      },
+    };
+
+    const state = authReducer({ logged: false }, action);
+    expect(state).toEqual({
+      logged: true,
+      user: action.payload,
+    });
+  });
+
+  test("debe de hacer logout, borrar el name del usuario y logged en false ", () => {
+    const action = {
+      type: types.logout,
+    };
+
+    const state = {
+      logged: true,
+      user: {
+        name: "Juan",
+        id: "123",
+      },
+    };
+    const newState = authReducer(state, action);
+    expect(newState).toEqual({ logged: false });
+  });
+});
