69e3aeca69997c4066c6078da15532f1df3f77c1
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-12-04 19:00:58 -0500

    Pruebas Unitarias
    Se termina con las pruebas unitarias

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -34,7 +34,7 @@ export const SearchPage = () => {
         <div className="col-4">
           <h4>Searching</h4>
           <hr />
-          <form onSubmit={onSearchSubmit}>
+          <form onSubmit={onSearchSubmit} aria-label='form'>
             <input
               type="text"
               placeholder="search a hero"
@@ -52,7 +52,7 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
           <div
-          aria-label="alert-danger"
+          
             className="alert alert-primary animate__animated animate__fadeIn"
             style={{ display: showSearch ? "" : "none" }}
           >
@@ -60,6 +60,7 @@ export const SearchPage = () => {
           </div>
 
           <div
+             aria-label="alert-danger"
             className="alert alert-danger animate__animated animate__fadeIn"
             style={{ display: showError ? "" : "none" }}
           >
--- tests/heroes/pages/SearchPage.test.jsx
+++ tests/heroes/pages/SearchPage.test.jsx
@@ -1,9 +1,19 @@
 /* eslint-disable no-undef */
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
 import { SearchPage } from './../../../src/heroes/pages/SearchPage';
 
+const mockedUseNavigat = jest.fn()
+
+jest.mock('react-router-dom' , () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedUseNavigat
+}));
+
 describe('Pruebas en el search page', () => {
+
+    beforeEach(() => jest.clearAllMocks());
+
   test('debe de mostrarse correctamente con valores por defectos', () => {
     const {container} = render(
         <MemoryRouter>
@@ -31,5 +41,37 @@ describe('Pruebas en el search page', () => {
     const alert = screen.getByLabelText('alert-danger')
     expect(alert.style.display).toBe('none')
   })
+
+  test('debe de mostrar un error si no se encuentra el heroe', () => {
+    render(
+        <MemoryRouter initialEntries={['/search?q=batman123']}>
+            <SearchPage/>
+        </MemoryRouter>
+    )
+
+    screen.debug()
+    const alert = screen.getByLabelText('alert-danger')
+    expect(alert.style.display).toBe('')
+  })
+
+  test('debe de llamar el navigate a la pantalla nueva ', () => {
+    render(
+        <MemoryRouter initialEntries={['/search?q=batman123']}>
+            <SearchPage/>
+        </MemoryRouter>
+    )
+
+    //llenamos el input de informacion
+    const input = screen.getByRole('textbox')
+    fireEvent.change(input, {target: {name: 'searchText', value: 'superman'}})
+
+    //hacemos el submit del formulario que nos debe de llevar a la otra pagina
+    const form = screen.getByRole('form')
+    fireEvent.submit(form)
+
+    expect(mockedUseNavigat).toHaveBeenCalledWith('?q=superman')
+  })
+  
+  
   
 })
